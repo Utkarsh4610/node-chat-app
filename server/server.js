5fb3110903dce8be41ca50b23f8b39c2381a642c
Utkarsh Kumar <utkarsh.kumar4610@gmail.com>
2018-11-21 16:08:59 +0530

    'updatedmessagesystem'

--- server/server.js
+++ server/server.js
@@ -13,7 +13,18 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New user connected.');
- 
+    //when new user joins
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'New User joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('CreateMessage',(message)=>{
         console.log('CreateMessage',message)
         io.emit('newMessage',{
@@ -21,6 +32,11 @@ io.on('connection',(socket)=>{
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
     socket.on('disconnect',()=>{
         console.log('User was disconnected.');
